9cc7e6b73270abdbb2f4cbce222283b29012b52c
AlejandroMixteco <alejandro.mixteco.24e@utzmg.edu.mx>
2026-09-04 18:03:19 -0600

    Bloqueo de login por intentos fallidos

--- routes/api_admin.php
+++ routes/api_admin.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Admin\AdminServicioController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/admin/login', [AuthController::class, 'login']);
+Route::post('/admin/login', [AuthController::class, 'login'])->middleware('throttle:5,1');
 
 Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
